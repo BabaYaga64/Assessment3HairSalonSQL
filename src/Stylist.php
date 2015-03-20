a314575fa4115bfd9c77a50ce2cc9936a77f9699
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-20 13:46:19 -0700

    Add getAll(), deleteAll() and find() methods to Stylist.php.

--- src/Stylist.php
+++ src/Stylist.php
@@ -5,6 +5,7 @@
 		private $name;
 		private $id;
 
+//Constructor
 
 		function __construct($name, $id = null)
 		{
@@ -29,7 +30,7 @@
 		function getId()
 		{
 
-			return $this->$id;
+			return $this->id;
 		}
 
 		function setId($new_id)
@@ -47,6 +48,17 @@
 
 		}
 
+		// function findClients()
+		// {
+
+		// 	$found_clients = array();
+		// 	$table_matches = $GLOBALS['DB']->query("SELECT * FROM clients WHERE stylist_id = {$this->getId()};");
+			
+		// }
+
+//STATIC FUNCTIONS (ACT ON ALL INSTANCES OF CLASS)
+
+
 		//READ: extracts and reads all content in all table rows in database
 		static function getAll()
 		{
@@ -62,6 +74,7 @@
 
 		}
 
+
 		//DELETE: removes ALL content from database
 		static function deleteAll()
 		{
@@ -70,9 +83,31 @@
 
 		}
 
+		static function find($stylist_search_id)
+		{
+			$found_stylist = null;
+			$all_stylists = Stylist::getAll();
+			foreach ($all_stylists as $current_stylist) {
+				$current_id = $current_stylist->getId();
+				if ($current_id = $stylist_search_id) {
+					$found_stylist = $current_stylist;
+				}//Ends if
+			return $found_stylist;
+				
+			}//Ends foreach
+
+		}
 
 
-		
+
+
+
+
+
+
+
+
+
 
 
 
